7d41bc96bd20eb9ba130970dbcd79337224b8a3c
maitatab <mairachinask8@gmail.com>
2022-02-28 13:32:33 -0600

    cambio

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    include 'db_connection.php';
+
+    $conn = OpenCon();
+    
+
+    $sql = "SELECT * FROM users";
+    /*where name = '".$_POST["nickname"]."' and password = '".$_POST["password"]."'";*/
+    
+
+    if($result = $conn->query($sql))
+    {
+        //printf("La selección devolvió %d filas. \n", $resultado->num_rows);
+        /* Liberar el conjunto de resultados */
+        for ($set = array(); $row = $result ->fetch_assoc(); $set[] = $row);
+        CloseCon($conn);
+        //echo json_encode(array ('result' => $result->num_rows));
+        echo json_encode($set);
+    }
+
+?>
